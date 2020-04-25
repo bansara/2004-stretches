e3440b47a388b9bb8acf2205823879f3706cddf5
Josh Geisler <joshuageisler@gmail.com>
2020-04-25 09:30:55 -0400

    finished?

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,11 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+    if(isNaN(num1) || isNaN(num2) || arguments.length !== 2) {
+        throw "multiply take exactly two numbers";
+    }
+    return num1 * num2;
+};
 
 module.exports = { multiply };
+
--- questions/01_multiply/multiply.test.js
+++ questions/01_multiply/multiply.test.js
@@ -4,23 +4,27 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(multiply('mike', 'joe')).toThrow();
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(multiply(1, 2, 3)).toThrow();
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(4, 5)).toBe(20);
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(10, 10)).toBe(100);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(multiply(1, 50)).toBe(50);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
     // test some random values
+    const random1 = Math.random() * 10;
+    const random2 = Math.random() * 5;
+    const result = random1 * random2;
+    expect(multiply(random1, random2)).toBe(result);
   });
 });
