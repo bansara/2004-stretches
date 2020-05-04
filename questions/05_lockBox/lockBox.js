c25ec811056cbf4f35110731542b7088f448223d
Josh Geisler <joshuageisler@gmail.com>
2020-05-04 18:54:44 -0400

    all specs passed

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,5 +1,34 @@
-const lockBox = () => {
+const lockBox = (code, msg) => {
   // write code here
+  if(typeof code !== 'number' || typeof msg !== 'string') {
+    throw 'enter a number and a string';
+  }
+  return {
+    modCode(pw, newCode) {
+      if(pw === code) {
+        code = newCode;
+        return code;
+      } else {
+        throw 'incorrect password'
+      }
+    },
+    modMessage(pw, newMsg) {
+      if (pw === code) {
+        message = newMsg;
+        return message;
+      } else {
+        throw 'incorrect password'
+      }
+    },
+    revealMessage(pw) {
+      if (pw === code) {
+        return message;
+      } else {
+        throw 'incorrect password'
+      }
+    } 
+
+  }
 };
 
 module.exports = { lockBox };
