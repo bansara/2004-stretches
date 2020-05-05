1db442d4dc10946da11c356bf34a88789a1cb49b
Josh Geisler <joshuageisler@gmail.com>
2020-05-05 10:14:47 -0400

    specs passed

--- questions/06_zip/zip.js
+++ questions/06_zip/zip.js
@@ -1,14 +1,15 @@
-/*
-write the zip function
-zip([{x: 1}, {x: 2}]) => {x: 3}
-zip([
-  {x: 1, y: 2}, 
-  {x: 2, z: 1},
-  {z: 2, y: 1}]) => {x: 3, y: 3, z: 3}
-*/
-
 function zip(objs) {
-  // YOUR CODE
+  return objs.reduce((a,c) => {
+    const keys = Object.keys(c);
+    for( let key of keys ) {
+      if(a.hasOwnProperty(key)) {
+        a[key] += c[key]
+      } else {
+        a[key] = c[key]
+      }
+    }
+    return a;
+  }, {})
 }
 
 module.exports = { zip };
