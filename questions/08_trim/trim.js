abf5d4a5f25cb9fd99f7800f5178554dac3b2ebb
Josh Geisler <joshuageisler@gmail.com>
2020-05-07 10:39:05 -0400

    specs passed

--- questions/08_trim/trim.js
+++ questions/08_trim/trim.js
@@ -1,7 +1,13 @@
 // trim takes an object and removes any keys that have undefined or null values
 
 const trim = obj => {
-  // YOUR CODE
+  const trimmedObj = {};
+  for(let prop in obj) {
+    if (obj[prop] !== undefined && obj[prop] !== null){
+      trimmedObj[prop] = obj[prop]
+    }
+  }
+  return trimmedObj;
 };
 
 module.exports = { trim };
