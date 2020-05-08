f780dbcfa516b29114c3833f7ef28d5a2fb791eb
Josh Geisler <joshuageisler@gmail.com>
2020-05-08 11:08:24 -0400

    specs passed

--- questions/09_diceRoller/DiceRoller.js
+++ questions/09_diceRoller/DiceRoller.js
@@ -4,6 +4,23 @@
 
 class DiceRoller {
   //YOUR CODE
+  constructor(sides, numDice) {
+    if(typeof sides !== 'number' || typeof numDice !== 'number' || sides < 1 || numDice < 1) {
+      throw 'Input invalid.'
+    }
+    this.sides = sides;
+    this.numDice = numDice;
+    this.history = [];
+  }
+  roll() {
+    const result = []
+    for (let i = 0; i < this.numDice; i++) {
+      result.push( (Math.floor(Math.random() * this.sides ) + 1))
+    }
+    this.history.push(result);
+    // console.log(result)
+    return result;
+  }
 }
 
 module.exports = { DiceRoller };
