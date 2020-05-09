388747a545bec3ef48b36f2390bce7df8c3f7e16
Josh Geisler <joshuageisler@gmail.com>
2020-05-09 09:07:46 -0400

    specs passed

--- questions/10_splatter/splatter.js
+++ questions/10_splatter/splatter.js
@@ -1,7 +1,13 @@
 //write the splatter function
 
-const splatter = () => {
+const splatter = (...args) => {
   //write code here
+  return [...args].reduce((a,c) => {
+    for (let val in c) {
+    a += c[val]
+    }
+    return a
+  }, 0)
 };
 
 module.exports = { splatter };
