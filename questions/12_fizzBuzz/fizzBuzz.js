951279dace8d86a6e25eb18e4fc9a67acc32f1f6
Josh Geisler <joshuageisler@gmail.com>
2020-05-11 18:45:52 -0400

    specs passed

--- questions/12_fizzBuzz/fizzBuzz.js
+++ questions/12_fizzBuzz/fizzBuzz.js
@@ -1,4 +1,18 @@
-const fizzBuzz = () => {
+const fizzBuzz = (num) => {
+  if (typeof num !== 'number' || num % 1 !== 0 || num < 1) throw 'input is not an integer'
   //YOUR CODE HERE
+  let output = []
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      output.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      output.push('Buzz');
+    } else if (i % 3 === 0) {
+      output.push('Fizz');
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
 };
 module.exports = { fizzBuzz };
