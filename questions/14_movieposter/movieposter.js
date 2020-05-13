158fcacecc4864322a33dbf7cd7a65cc65eb73aa
Josh Geisler <joshuageisler@gmail.com>
2020-05-13 18:51:48 -0400

    blah

--- questions/14_movieposter/movieposter.js
+++ questions/14_movieposter/movieposter.js
@@ -7,8 +7,28 @@
 // * Play *
 // ********
 
-const movieposter = () => {
+const movieposter = (array) => {
   //write code here
+  if(!Array.isArray(array) || array.length < 1) throw new Error('this input sucks.');
+  let longestWord = 0;
+  array.forEach(word => {
+    if(word > longestWord) {
+      longestWord = word.length;
+    }
+  })
+  let output = '';
+  for (let i = 0; i < array.length; i++) {
+    if(i === 0) {
+      output = '*'.repeat(longestWord + 4) + '\n';
+    } 
+    output += `* ${array[i]}${' '.repeat(longestWord - array[i].length)} *`;
+    if(i === array.length - 1) {
+      
+      output = '*'.repeat(longestWord + 4);
+    }
+
+  }
+  return output;
 };
 
 module.exports = { movieposter };
