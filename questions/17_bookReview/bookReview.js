23751331b98ab1ec00d33c14ab2364b7f65a43e1
Josh Geisler <joshuageisler@gmail.com>
2020-05-16 09:09:52 -0400

    specs passed

--- questions/17_bookReview/bookReview.js
+++ questions/17_bookReview/bookReview.js
@@ -1,2 +1,16 @@
-class BookReview {}
+class BookReview {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+        this.notes = [];
+        this.rating = null;
+    }
+    addNotes(page, note) {
+        this.notes[page] = this.notes[page] ? `${this.notes[page]} ${note}` : note;
+    }
+    addRating(rating) {
+        if(rating > 5 || rating < 0) throw new Error('Rating is between 0 and 5');
+        this.rating = rating;
+    }
+}
 module.exports = { BookReview };
