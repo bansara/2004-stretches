391cfcb1907ba57b1f93d065b3565da38d1312e8
Josh Geisler <joshuageisler@gmail.com>
2020-05-17 17:56:24 -0400

    specs passed

--- questions/18_arrayexchange/arrayexchange.js
+++ questions/18_arrayexchange/arrayexchange.js
@@ -1,6 +1,16 @@
 //arrayexchange should exchange the elements of two arrays in-place in a way that their new content is also reversed.
 //restricted use: reverse()
 
-const arrayexchange = () => {};
+const arrayexchange = (a1,a2) => {
+    if(!Array.isArray(a1) || !Array.isArray(a2)) throw new Error("this is not the droid you're looking for");
+    const a1l = a1.length;
+    const a2l = a2.length;
+    for(let i = 0; i < a1l; i++) {
+        a2.unshift(a1.shift());
+    }
+    for(let i = 0; i < a2l; i++) {
+        a1.push(a2.pop())
+    }
+};
 
 module.exports = { arrayexchange };
