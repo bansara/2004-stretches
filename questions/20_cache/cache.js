a8590ff07aef566cd4d43474ccca319a34f71272
Josh Geisler <joshuageisler@gmail.com>
2020-05-19 19:58:44 -0400

    passed

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -1,7 +1,25 @@
 // see test specs
 
-function cache() {
-  // YOUR CODE
+function cache(f) {
+  if (typeof f !== 'function') {
+    throw new Error('Input must be a function.');
+  }
+  const cachedArg = [];
+  const cachedResult = [];
+  return (x) => {
+    if(cachedArg.includes(x)){
+      const index = cachedArg.indexOf(x);
+      return cachedResult[index];
+    }
+    else {
+      cachedArg.push(x);
+      const result = f(x);
+      cachedResult.push(result);
+      return result;
+    }
+
+  }
 }
+
 
 module.exports = { cache };
