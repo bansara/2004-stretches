a09dbd26f7be7813218a685b5f1bf0c78003127b
Josh Geisler <joshuageisler@gmail.com>
2020-05-22 15:17:49 -0400

    specs passed

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,20 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (fn) => {
+    if (typeof fn !== 'function') {
+        throw 'not a function';
+    }
+    return (a) => {
+        if(typeof a !== 'number') {
+            throw 'not a number';
+        }
+        return (b) => {
+            if(typeof b !== 'number') {
+                throw 'not a number';
+            } 
+            return fn(a,b)
+        }
+    }
+};
 
 module.exports = { extensions };
