c68bbd7984a17c31870c20cf387ebae690f402c7
Josh Geisler <joshuageisler@gmail.com>
2020-05-21 22:28:42 -0400

    specs passed

--- questions/22_moveZeroes/moveZeroes.js
+++ questions/22_moveZeroes/moveZeroes.js
@@ -1,5 +1,13 @@
 const moveZeroes = (arr) => {
   //YOUR CODE HERE
+  const numZeros = arr.filter(n => n===0).length;
+  while(arr.slice(-numZeros).join("") !== '0'.repeat(numZeros))
+  {
+    let i = arr.indexOf(0);
+    arr.splice(i,1);
+    arr.push(0);
+  } 
+  return arr;
 };
 
 module.exports = { moveZeroes };
