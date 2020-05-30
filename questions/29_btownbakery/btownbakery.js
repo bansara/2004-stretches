d074da5c7c57f6ba38e131e9c5c29dd17d08882c
Josh Geisler <joshuageisler@gmail.com>
2020-05-30 09:14:03 -0400

    baked

--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,7 +1,16 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = () => {
+const btownbake = (recipe,supplies) => {
   //enter code here
+  if(typeof recipe !== 'object' || typeof supplies !== 'object') throw 'you are not the father';
+  let stuff = [];
+  for (let ingredient in recipe) {
+    if(!Object.keys(supplies).includes(ingredient)) {
+      return 0;
+    }
+    stuff.push(Math.floor(supplies[ingredient]/recipe[ingredient]))
+  }
+  return Math.min(...stuff);
 };
 
 module.exports = { btownbake };
