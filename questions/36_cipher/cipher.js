c59735366957a58fa25ad76f8aecca5fbb3d86e5
Josh Geisler <joshuageisler@gmail.com>
2020-06-07 19:05:10 -0400

    encrypted

--- questions/36_cipher/cipher.js
+++ questions/36_cipher/cipher.js
@@ -1,5 +1,16 @@
-const encrypt = () => {
-  //YOUR CODE HERE
+const encrypt = (str) => {
+  const arrOfWords = str.split(' ');
+  let outputArr = [];
+  arrOfWords.forEach(word => {
+    const wordArr = word.split('');
+    const last = wordArr.pop();
+    const second = wordArr.splice(1, 1).join('');
+    wordArr.splice(1, 0, last);
+    wordArr.push(second);
+    wordArr[0] = wordArr[0].charCodeAt(0);
+    outputArr.push(wordArr.join(''))
+  })
+  return outputArr.join(' ');
 };
 
 module.exports = { encrypt };
