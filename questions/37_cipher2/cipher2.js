d0c0bef7c0db1a991e01c3bc7042a489a68bf99b
Josh Geisler <joshuageisler@gmail.com>
2020-06-08 18:56:15 -0400

    almost there

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,4 +1,21 @@
-const decypher = () => {
+const decypher = (str) => {
   //YOUR CODE HERE
+  const strArr = str.split(' ');
+  const result = [];
+  strArr.forEach(word => {
+    let wordArr = word.split('');
+    let decoded = ''
+    const num = word.match(/[0-9]+/)[0];
+    const firstLetter = String.fromCharCode(num * 1)
+    const secondLetter = wordArr.pop();
+    const restOfWord = wordArr.splice(num.length);
+
+    decoded += firstLetter;
+    decoded += secondLetter;
+    decoded += restOfWord;
+    result.push(decoded)
+
+  })
+  return result.join(' ');
 };
 module.exports = { decypher };
