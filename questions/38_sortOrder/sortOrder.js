046a6fcb09679a925e4b26565fb6d15627c2c8ce
Josh Geisler <joshuageisler@gmail.com>
2020-06-09 16:50:45 -0400

    sorted

--- questions/38_sortOrder/sortOrder.js
+++ questions/38_sortOrder/sortOrder.js
@@ -3,8 +3,9 @@ SortOrder takes an array of objects, a key to be sorted on
 and a key for the return value. 
 */
 
-function sortOrder() {
-  // YOUR CODE
+function sortOrder(arr, key1, key2) {
+  return arr.sort((a, b) => a[key1] < b[key1] ? -1 : 1)
+    .map(obj => obj[key2])
 }
 
 module.exports = { sortOrder };
