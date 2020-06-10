5833259a8b6093db15a22550f18f701868d22c49
Josh Geisler <joshuageisler@gmail.com>
2020-06-10 18:58:15 -0400

    almost there

--- questions/39_getstudents/getstudents.js
+++ questions/39_getstudents/getstudents.js
@@ -4,8 +4,24 @@ const axios = require('./axios');
 //returning that specific student as specified in the test
 //* you don't need to NPM i axios for this test... Only work within this file and use promises
 
-const getStudent = () => {
+const getStudent = (student) => {
   //code in here
+  if (typeof student !== 'string') throw new Error('blargh');
+  const output = {};
+  const URL = 'https://fullstack.com/api/cohort2004';
+  axios.get(`${URL}`)
+    .then(students => students.data)
+    .then(data => {
+      let count, pupil;
+      for (let id in data) {
+        count++;
+        if (data[id][name] === student) pupil = data[id[name]]
+      }
+      output.count = count;
+      output.student = pupil;
+
+    }).catch(e => console.log(e))
+  return output;
 };
 
 module.exports = { getStudent };
