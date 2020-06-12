a1f37fc41e297b8d656b4dd77ee1c32053ddefdd
Josh Geisler <joshuageisler@gmail.com>
2020-06-11 21:13:27 -0400

    boxed

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -1,3 +1,13 @@
 //YOUR CODE HERE
 
+class Box {
+    constructor(color, capacity) {
+        this.color = color;
+        this.contents = [];
+        this.capacity = capacity;
+    }
+    pack(color, capacity) {
+        this.contents.push(new Box(color, capacity))
+    }
+}
 module.exports = { Box };
