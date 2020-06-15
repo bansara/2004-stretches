257e4afa07382692cde3d1e205649bc0f62f4733
Josh Geisler <joshuageisler@gmail.com>
2020-06-15 09:04:04 -0400

    mmm pizza

--- questions/43_makeyourown/makeyourown.js
+++ questions/43_makeyourown/makeyourown.js
@@ -4,8 +4,11 @@
 // Jest documentation: https://github.com/sapegin/jest-cheat-sheet
 // submit the stretch with solution code and test specs passing
 
-const makeyourown = () => {
-  console.log('make your own stretch!');
+const makeyourown = (pizza = 'yesss feed me', anchovies = 'nooo kill them with fire') => {
+  return {
+    pizza,
+    anchovies
+  };
 };
 
 module.exports = { makeyourown };
--- questions/43_makeyourown/makeyourown.spec.js
+++ questions/43_makeyourown/makeyourown.spec.js
@@ -1,7 +1,19 @@
 const { makeyourown } = require('./makeyourown');
 
 describe('makeyourown functionality', () => {
-  it('your first test spec', () => {
-    expect(makeyourown()).toEqual();
+  it('returns an object', () => {
+    expect(typeof makeyourown(5)).toEqual('object');
+  });
+  it('assigns default values to arguments if none are given', () => {
+    expect(makeyourown()).toMatchObject({
+      pizza: 'yesss feed me',
+      anchovies: 'nooo kill them with fire'
+    });
+  });
+  it('assigns arguments to values of the object', () => {
+    expect(makeyourown('yes please', 'no!!')).toMatchObject({
+      pizza: 'yes please',
+      anchovies: 'no!!'
+    });
   });
 });
