6db329853c8e9a11b01f43f969a03946257ce2bd
Josh Geisler <joshuageisler@gmail.com>
2020-06-15 18:50:29 -0400

    iterated

--- questions/44_iterator/iterator.js
+++ questions/44_iterator/iterator.js
@@ -3,6 +3,26 @@
 
 class Iterator {
   //write code here
+  constructor(arr) {
+    if (!Array.isArray(arr)) {
+      throw new Error('not an array')
+    };
+    this.arr = arr;
+    this.index = 0;
+    this.done = false;
+  }
+  next() {
+
+    this.done = this.index < this.arr.length ? false : true;
+    const output = {
+      done: this.done,
+      value: this.arr[this.index]
+    }
+    this.index++;
+    return output;
+
+  }
 }
+
 
 module.exports = { Iterator };
