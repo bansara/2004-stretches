2eeda09348749bfcae5b258abb0fa76220b892dd
Josh Geisler <joshuageisler@gmail.com>
2020-06-23 17:12:07 -0400

    calculated

--- questions/49_calc/calc.js
+++ questions/49_calc/calc.js
@@ -2,6 +2,23 @@
 
 class chainCalc {
   //write code here
+  constructor(n) {
+    if (typeof n !== 'number') throw 'what do you think this is, algebra?';
+    this.result = n;
+  }
+  add(n) {
+    this.result += n;
+    return this;
+  }
+  sub(n) {
+    this.result -= n;
+    return this;
+  }
+  mul(n) {
+    this.result *= n;
+    return this;
+  }
+
 }
 
 module.exports = { chainCalc };
