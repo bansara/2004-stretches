1a22ccda478a08fb8928e31054fb977fa0a4aab9
Josh Geisler <joshuageisler@gmail.com>
2020-06-27 09:13:10 -0400

    specs passed

--- questions/51_reducer/reducer.js
+++ questions/51_reducer/reducer.js
@@ -19,8 +19,26 @@ const initialState = [
 const reducer = (state = initialState, action) => {
   //ADD YOUR CODE HERE
   switch (action.type) {
+    case 'ADD_TODO': {
+      const todo = {
+        text: action.text,
+        completed: false,
+        id: state.length
+      }
+      state = [todo, ...state];
+      return state;
+    }
+    case 'DELETE_TODO': {
+      state = state.filter(item => item.id !== action.id);
+      return state;
+    }
+    case 'COMPLETE_TODO': {
+      const itemToModify = state.find(item => item.id === action.id);
+      itemToModify.completed = true;
+      return state;
+    }
     default:
-      return;
+      return state;
   }
 };
 
