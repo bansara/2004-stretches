671ee811491ba167f9e8fc0b0b3a3ed6935baca7
Josh Geisler <joshuageisler@gmail.com>
2020-07-13 18:56:12 -0400

    nope

--- questions/53_balancedArr/balancedArr.js
+++ questions/53_balancedArr/balancedArr.js
@@ -1,5 +1,25 @@
-const balanced = () => {
+const balanced = (arr) => {
   //YOUR CODE HERE
+  const left = 0;
+  const right = arr.length - 1;
+  let sumL, sumR;
+  while (left < right) {
+    sumL += arr[left];
+    sumR += arr[right];
+    if (sumL < sumR) {
+      left++;
+    } else if (sumR < sumL) {
+      right--;
+    } else if (sumR === sumL) {
+      if (right - left === 2) return left + 1;
+      else if (right - left === 1) return left;
+      else {
+        left++;
+        right--;
+      };
+    }
+  }
+  return -1;
 };
 
 module.exports = balanced;
