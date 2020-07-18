cbc2ff84b04b737f97c25d8ba92e3cc95c7bebd7
Josh Geisler <joshuageisler@gmail.com>
2020-07-18 09:19:37 -0400

    nope

--- questions/54_fold/fold.js
+++ questions/54_fold/fold.js
@@ -1,5 +1,25 @@
-const fold = () => {
+const fold = (arr, int) => {
   //YOUR CODE HERE
+  const result = [];
+  const arrCopy = [...arr]
+  const shiftPop = (arr) => {
+    result.push(arr.shift() + arr.pop());
+  }
+
+  while (arrCopy.length && int) {
+    int--;
+    if (arrCopy.length % 2 === 0) {
+      shiftPop(arrCopy);
+    }
+    else {
+      if (arrCopy.length > 1) {
+        shiftPop(arrCopy)
+      } else {
+        result.push(arrCopy.pop())
+      }
+    }
+  }
+  return result;
 };
 
 module.exports = fold;
